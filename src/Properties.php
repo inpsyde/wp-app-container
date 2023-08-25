6f332bad41d7dbffad2cab8e21c637e97d91e11b
tfrommen <info@tfrommen.de>
2023-08-25 14:45:38 +0200

    Import Locations and add @throws annotation

--- src/Properties.php
+++ src/Properties.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Inpsyde\App;
 
+use Inpsyde\App\Config\Locations;
 use Inpsyde\Modularity\Properties\BaseProperties;
 use Inpsyde\Modularity\Properties\LibraryProperties;
 
 class Properties extends BaseProperties
 {
     /**
-     * @param Config\Locations $locations
+     * @param Locations $locations
      * @param bool $enableDebug
+     * @throws \Exception
      */
-    public function __construct(Config\Locations $locations, bool $enableDebug)
+    public function __construct(Locations $locations, bool $enableDebug)
     {
         $path = dirname(__DIR__);
         parent::__construct(
